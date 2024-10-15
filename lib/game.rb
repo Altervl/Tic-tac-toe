f20bbd24564aa2c52bed5e07798f39321f08335b
Altervl <62514019+Altervl@users.noreply.github.com>
2024-10-15 18:30:12 +0300

    Fix the gameboard creation

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ class Game
   attr_accessor :board
 
   def initialize
-    @board = Array.new(3, Array.new(3, nil))
+    @board = Array.new(3) { Array.new(3) } # new array per row instead of reference
     @player_X = Player.new('X')
     @player_0 = Player.new('0')
     @winner = nil
